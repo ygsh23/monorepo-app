e96192b9c7f74b8aca43269272bdc9d70f823f44
Yogesh <ysaroj@qasource.com>
2024-12-17 16:30:16 +0530

    update

---
+++ libs/shared-lib/src/lib/shared-lib.tsx
@@ -0,0 +1,11 @@
+import styles from './shared-lib.module.css';
+
+export function SharedLib() {
+  return (
+    <div className={styles['container']}>
+      <h1>Welcome to SharedLib!</h1>
+    </div>
+  );
+}
+
+export default SharedLib;
